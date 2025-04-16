22eb8c3e2d629dee7f6af01b43a0fb2f21b8776e
Blck Strt <blockstarter34@gmail.com>
2025-04-16 00:33:44 -0500

    Homepage

---
+++ web/Components/Cards.jsx
@@ -0,0 +1,14 @@
+import { Card } from "./retroui/Card";
+
+
+const BasicCard = () => {
+  return (
+    <Card className="w-full max-w-sm shadow-lg p-2">
+        <h2 className="text-xl font-bold mb-2">Joshua Levy</h2>
+        <p>Welcome to the internet.</p>
+     
+    </Card>
+  );
+};
+
+export default BasicCard;
